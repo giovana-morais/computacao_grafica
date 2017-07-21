2ca8d8ed932d5611c1299ff5505b0fc9e7d2bce6
Ingrid Santos <ingridmsantosp13@gmail.com>
2017-07-21 11:15:41 -0300

    4 yodinhas

--- trabalho_final/js/functions.js
+++ trabalho_final/js/functions.js
@@ -1,12 +1,12 @@
 var clock, container, camera,camera_2, scene, renderer, controls, listener;
 var id, i=0;
-var ground, storminho, yoda,cloneyoda;
+var ground, storminho, yoda,cloneyoda,cloneyoda2,cloneyoda3;
 var light;
 var textureLoader = new THREE.TextureLoader();
 var loader = new THREE.JSONLoader();
 var keyboard = new THREEx.KeyboardState();
 var isLoaded = false;
-var action = {}, mixer_storminho,mixer_yoda,mixer_clone;
+var action = {}, mixer_storminho,mixer_yoda,mixer_clone,mixer_clone2,mixer_clone3;
 
 // arrumar os nomes qnd der certo
 var arrAnimations = [
@@ -194,8 +194,67 @@ function init () {
 
   });
 
+  loader.load('yoda.json', function (geometry, materials) {
+    materials.forEach(function (material) {
+      material.skinning = true;
+    });
+    cloneyoda2 = new THREE.SkinnedMesh(
+      geometry,
+      new THREE.MeshFaceMaterial(materials)
+    );
 
-  var objLoader = new THREE.OBJLoader();
+    cloneyoda2.position.x = 2;
+    cloneyoda2.position.y = -0.5;
+    cloneyoda2.position.z = -2;
+    cloneyoda.updateMatrix();
+
+    mixer_clone2 = new THREE.AnimationMixer(cloneyoda2);
+
+    action.dancayoda = mixer_clone2.clipAction(geometry.animations[ 0 ]);
+    action.dancayoda.setEffectiveWeight(1);
+    action.dancayoda.enabled = true;
+        scene.add(cloneyoda2);
+
+    window.addEventListener('resize', onWindowResize, false);
+    window.addEventListener('click', onDoubleClick, false);
+    animate();
+
+    isLoaded = true;
+    action.dancayoda.play();
+
+  });
+  loader.load('yoda.json', function (geometry, materials) {
+    materials.forEach(function (material) {
+      material.skinning = true;
+    });
+    cloneyoda3 = new THREE.SkinnedMesh(
+      geometry,
+      new THREE.MeshFaceMaterial(materials)
+    );
+
+    cloneyoda3.position.x = -2;
+    cloneyoda3.position.y = -0.5;
+    cloneyoda3.position.z = -2;
+    cloneyoda.updateMatrix();
+
+    mixer_clone3 = new THREE.AnimationMixer(cloneyoda3);
+
+    action.dancayoda = mixer_clone3.clipAction(geometry.animations[ 0 ]);
+    action.dancayoda.setEffectiveWeight(1);
+    action.dancayoda.enabled = true;
+        scene.add(cloneyoda3);
+
+    window.addEventListener('resize', onWindowResize, false);
+    window.addEventListener('click', onDoubleClick, false);
+    animate();
+
+    isLoaded = true;
+    action.dancayoda.play();
+
+  });
+
+// Tentativa de colocar um outro obj na cena
+/*  var objLoader = new THREE.OBJLoader();
   objLoader.setPath('imgs/JAWA/');
   objLoader.load('Star_wars_JAWA.obj',function (object){
     object.position.x = 4;
@@ -212,7 +271,7 @@ function init () {
   mtlLoader.load('Star_wars_JAWA.mtl', function (materials) {
     materials.preload();
     objLoader.setMaterials(materials);
-  });
+  }); */
 }
 
 function onWindowResize () {
@@ -233,8 +292,7 @@ function animate () {
 
 }
 
-// Pode mover um de cada vez, pressionando 1,2 ou 3
-// Pode mover todos ao mesmo tempo, mantendo os botões pressionados
+// Pode mover um de cada vez ou combinação de até 3 teclas (mais que isso não consegui combinar)
 
 function render () {
     var delta = clock.getDelta();
@@ -246,17 +304,29 @@ function render () {
       cam = camera;
     }
 
+    // Pressionar 1 pra animação principal aka storminho
 
     if(keyboard.pressed("1")) {
       renderer.render(scene, cam);
         mixer_storminho.update(delta);
     }
 
-    if(keyboard.pressed("2")) {
+    // Como mexer nos yodinhas : da esquerda pra direita A,Q,W,S;
+    //ou seja, os dois mais pra cima são Q e W e os de baixo A e S
+
+    if(keyboard.pressed("a")) {
       renderer.render(scene, cam);
         mixer_yoda.update(delta);
     }
-    if(keyboard.pressed("3")) {
+    if(keyboard.pressed("q")) {
+      renderer.render(scene, cam);
+        mixer_clone3.update(delta);
+    }
+    if(keyboard.pressed("w")) {
+      renderer.render(scene, cam);
+        mixer_clone2.update(delta);
+    }
+    if(keyboard.pressed("s")) {
       renderer.render(scene, cam);
         mixer_clone.update(delta);
     }
