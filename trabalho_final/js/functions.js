b71718439c061713f3d625ebad7ff6cf3c1197d8
Giovana Morais <giovana.vmorais@gmail.com>
2017-07-21 04:31:35 -0300

    chão ok

--- trabalho_final/js/functions.js
+++ trabalho_final/js/functions.js
@@ -1,11 +1,11 @@
 var clock, container, camera, scene, renderer, controls, listener;
 var i;
-var ground, character;
+var ground, storminho, yoda;
 var light;
 var textureLoader = new THREE.TextureLoader();
 var loader = new THREE.JSONLoader();
 var isLoaded = false;
-var action = {}, mixer;
+var action = {}, mixer_storminho, mixer_yoda;
 
 // arrumar os nomes qnd der certo
 var arrAnimations = [
@@ -43,44 +43,47 @@ function init () {
   scene.add(light);
   scene.background = new THREE.Color(  0xffffff );
 
-/*  textureLoader.load('texturas/ground.png', function (texture) {
-    var geometry = new THREE.PlaneBufferGeometry(2, 2);
-    geometry.rotateX(-Math.PI / 2);
-    var material = new THREE.MeshBasicMaterial({ map: texture, transparent: true });
-    ground = new THREE.Mesh(geometry, material);
+    ground_textura = THREE.ImageUtils.loadTexture('texturas/ground.png');
+    ground_textura.wrapS = THREE.RepeatWrapping;
+    ground_textura.wrapT = THREE.RepeatWrapping;
+    ground_textura.repeat.x = 256;
+    ground_textura.repeat.y = 256;
+    
+    var ground_material = new THREE.MeshBasicMaterial({map: ground_textura, transparent: true});
+
+    var ground_geometria = new THREE.PlaneGeometry(400, 400);
+
+    ground = new THREE.Mesh(ground_geometria, ground_material);
+    ground.position.y = -1;
+    ground.rotation.x = -Math.PI/2;
+    ground.doubleSided = true;
     scene.add(ground);
 
-  }); */
+
   loader.load('Stormtrooper.json', function (geometry, materials) {
 
     materials.forEach(function (material) {
       material.skinning = true;
     });
-    character = new THREE.SkinnedMesh(
+    storminho = new THREE.SkinnedMesh(
       geometry,
       new THREE.MeshFaceMaterial(materials)
     );
 
-    // seta a posição inicial do personagem
-    character.position.x = 0;
-    character.position.y = 0;
-    character.updateMatrix();
+    storminho.position.x = 0;
+    storminho.position.y = 0;
+    storminho.updateMatrix();
 
-    mixer = new THREE.AnimationMixer(character);
-
-    action.danca = mixer.clipAction(geometry.animations[ 2 ]);
-
+    mixer_storminho = new THREE.AnimationMixer(storminho);
+    action.danca = mixer_storminho.clipAction(geometry.animations[ 2 ]);
     action.danca.setEffectiveWeight(1);
-
     action.danca.enabled = true;
-
-    scene.add(character);
+    scene.add(storminho);
 
     window.addEventListener('resize', onWindowResize, false);
     animate();
 
     isLoaded = true;
-
     action.danca.play();
   });
 
@@ -89,36 +92,28 @@ function init () {
     materials.forEach(function (material) {
       material.skinning = true;
     });
-    character = new THREE.SkinnedMesh(
+    yoda = new THREE.SkinnedMesh(
       geometry,
       new THREE.MeshFaceMaterial(materials)
     );
 
-    // seta a posição inicial do personagem
-    character.position.x = -4;
-    character.position.y = -2;
-    character.updateMatrix();
+    yoda.position.x = -4;
+    yoda.position.y = -0.5;
+    yoda.updateMatrix();
 
-    mixer = new THREE.AnimationMixer(character);
+    mixer_yoda = new THREE.AnimationMixer(yoda);
 
-
-    action.dancayoda = mixer.clipAction(geometry.animations[ 0 ]);
-
+    action.dancayoda = mixer_yoda.clipAction(geometry.animations[ 0 ]);
     action.dancayoda.setEffectiveWeight(1);
-
     action.dancayoda.enabled = true;
-
-    scene.add(character);
+    scene.add(yoda);
 
     window.addEventListener('resize', onWindowResize, false);
     animate();
 
     isLoaded = true;
-
     action.dancayoda.play();
   });
-
-
 }
 
 function onWindowResize () {
@@ -137,6 +132,7 @@ function animate () {
 
 function render () {
   var delta = clock.getDelta();
-  mixer.update(delta);
+  mixer_storminho.update(delta);
+  mixer_yoda.update(delta);
   renderer.render(scene, camera);
 }
